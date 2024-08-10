d6279110ea71da82661616950d2bab24a53bf1c8
wmatveev <matveev@matller.com>
2024-08-10 22:56:12 +0300

    testing

--- laser/laser_controller.go
+++ laser/laser_controller.go
@@ -20,12 +20,18 @@ func NewLaserController(address uint16) (*LaserController, error) {
 		return nil, err
 	}
 
+	log.Printf("---> 1: \n")
+
 	bus, err := i2creg.Open("/dev/i2c-1")
 	if err != nil {
 		return nil, err
 	}
 
+	log.Printf("---> 2: \n")
+
 	dev := i2c.Dev{Bus: bus, Addr: address}
+
+	log.Printf("---> 3: \n")
 
 	return &LaserController{
 		Address:    address,
@@ -39,11 +45,17 @@ func (lc *LaserController) StartPinsPolling() {
 	var readBuf [1]byte
 	var lastState bool
 
+	log.Printf("---> 4: \n")
+
 	for {
+		log.Printf("---> 5: \n")
+
 		if err := lc.Device.Tx(nil, readBuf[:]); err != nil {
 			log.Printf("Failed to read from device: %v\n", err)
 			break
 		}
+
+		log.Printf("---> 6: \n")
 
 		currentState := readBuf[0]&0x80 == 0x01
 
