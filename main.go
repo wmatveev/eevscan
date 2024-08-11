b3cdcddf02fc7069e63f7f65eb82e945d809e1c3
wmatveev <matveev@matller.com>
2024-08-11 21:06:50 +0300

    refactoring

--- main.go
+++ main.go
@@ -9,20 +9,28 @@ import (
 )
 
 func main() {
+	log.Println("---> main 1")
+
 	lc, err := laser.NewLaserController(0x21)
 	if err != nil {
 		log.Fatalf("Failed to initialize laser controller: %v", err)
 	}
+
+	log.Println("---> main 2")
 
 	sc, err := scanner.NewScannerController(0x20)
 	if err != nil {
 		log.Fatalf("Failed to initialize laser controller: %v", err)
 	}
 
+	log.Println("---> main 3")
+
 	pc, err := device.NewPortController()
 	if err != nil {
 		log.Fatalf("Failed to initialize port controller: %v", err)
 	}
+
+	log.Println("---> main 4")
 
 	stateManager := state.NewStateManager(lc, sc, pc)
 	stateManager.Start()
